e2e6f4e494423d6df0629e3e303addab0101c3e5
exussum <exussum@gmail.com>
2026-05-11 19:52:53 -0400

    Fix the stupid http 412; Turn off the progress bar only after the call ends; Stop orc before installing

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 . .venv-orc/bin/activate
+supervisorctl stop orc
 pip uninstall orc --yes
 pip install --index-url http://localhost:8080 orc
-supervisorctl stop orc
 supervisorctl start orc
 tail -f /var/log/orc.log
